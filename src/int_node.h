94f0b28d467db9e9ae61c5988244e147d0190664
QingZhiLiangCheng <884072265@qq.com>
2025-04-29 21:40:12 +0800

    feat: 新增Project1 TODO描述

---
+++ src/int_node.h
@@ -0,0 +1,14 @@
+//
+// Created by QingZhiLiangCheng on 2025/4/29.
+//
+
+#ifndef CHENGZILIST_INT_NODE_H
+#define CHENGZILIST_INT_NODE_H
+
+//TODO(Project 1 - Task 1) 实现IntNode类
+// - 实现一个名为IntNode的类 用于表示整数链表中的单个节点
+// - 每个节点包含两个主要部分：
+//      - `data`：存储整数值。
+//      - `next`：指向下一个节点的指针（对于最后一个节点，该值应为`nullptr`）
+
+#endif //CHENGZILIST_INT_NODE_H
---
+++ test/int_linked_test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by QingZhiLiangCheng on 2025/4/29.
+//
+//TODO(QingZhiLiangCheng) 实现IntLinkedList测试
+#include "gtest/gtest.h"
+//TODO(Project 1): 删除#indef标签
+#ifdef ENABLE_TESTS
+#endif
