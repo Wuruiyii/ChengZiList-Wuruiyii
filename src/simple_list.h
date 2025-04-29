97c7909fe7fdf990fb5f91bf8f9e8b4cac383056
wu <1509989070@qq.com>
2025-04-29 21:59:52 +0800

    feat: 实现simple_test

--- src/simple_list.h
+++ src/simple_list.h
@@ -11,5 +11,11 @@
 // 需要完成以下任务：
 // 1. 实现构造函数，用于初始化节点的数据和指针
 // 2. 实现size函数，用于计算链表的长度
-
+class SimpleList {
+public:
+    SimpleList* _next;
+    int _data;
+    SimpleList():_next(nullptr),_data(0) {}
+    SimpleList(SimpleList* next,int data):_next(next),_data(data) {}
+};
 #endif //CHENGZILIST_SIMPLE_LIST_H
